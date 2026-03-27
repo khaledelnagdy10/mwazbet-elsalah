33bcf96e9027496c91dd8fa08edf16d755fb7b57
khaledelnagdy10 <128099437+khaledelnagdy10@users.noreply.github.com>
2026-03-27 20:30:33 +0200

    create check_box_list_tile_container

---
+++ lib/features/home/presentation/widgets/check_box_list_tile_container.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:mwazbet_elsalah/features/home/domain/entities/entities.dart';
+import 'package:mwazbet_elsalah/features/home/presentation/widgets/check_box_list_tile.dart';
+
+class CheckBoxListTileContainer extends StatelessWidget {
+  const CheckBoxListTileContainer({super.key, required this.prayerTimesEntity});
+  final PrayerTimeEntity prayerTimesEntity;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(0.1),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Column(
+        children: [
+          CheckBoxListTile(prayer: 'Fajr', prayerTime: prayerTimesEntity.fajr),
+          CheckBoxListTile(prayer: 'Duhr', prayerTime: prayerTimesEntity.duhr),
+          CheckBoxListTile(prayer: 'Asr', prayerTime: prayerTimesEntity.asr),
+          CheckBoxListTile(
+            prayer: 'Maghrib',
+            prayerTime: prayerTimesEntity.maghrib,
+          ),
+          CheckBoxListTile(prayer: 'Isha', prayerTime: prayerTimesEntity.isha),
+        ],
+      ),
+    );
+  }
+}
