177b3b8b87f7a1d20c7c8d46a330f478b70cfbe8
khaledelnagdy10 <128099437+khaledelnagdy10@users.noreply.github.com>
2026-03-27 22:41:53 +0200

    create next prayer use case

--- lib/features/home/presentation/widgets/next_prayer_data.dart
+++ lib/features/home/presentation/widgets/next_prayer_data.dart
@@ -1,20 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mwazbet_elsalah/constants.dart';
+import 'package:mwazbet_elsalah/features/home/presentation/controller/prayer_time_cubit.dart';
 
 class NextPrayerData extends StatelessWidget {
   const NextPrayerData({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Positioned(
-          child: Text('Next Prayer', style: Style.bold20AllWhite(context)),
-        ),
-        Divider(color: Colors.grey, thickness: 1, indent: 125, endIndent: 125),
-        Text('ASR', style: Style.bold30AllWhite(context)),
-        Text('3:30:20', style: Style.bold30AllWhite(context)),
-      ],
+    return BlocBuilder<PrayerTimeCubit, PrayerTimeState>(
+      builder: (context, state) {
+        if (state is PrayerTimeFailure) {
+          return Center(child: Text('ERROR while get next prayer data'));
+        }
+        if (state is PrayerTimeSuccess) {
+          return Column(
+            children: [
+              Positioned(
+                child: Text(
+                  'Next Prayer',
+                  style: Style.bold20AllWhite(context),
+                ),
+              ),
+              Divider(
+                color: Colors.grey,
+                thickness: 1,
+                indent: 125,
+                endIndent: 125,
+              ),
+              Text(
+                state.nextPrayers['name'],
+                style: Style.bold30AllWhite(context),
+              ),
+              Text('3:30:20', style: Style.bold30AllWhite(context)),
+            ],
+          );
+        }
+
+        return SizedBox();
+      },
     );
   }
 }
