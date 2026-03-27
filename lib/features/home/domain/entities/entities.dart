b3c2f66583c2197b889ee433ea8558982c7780af
khaledelnagdy10 <128099437+khaledelnagdy10@users.noreply.github.com>
2026-03-27 20:34:51 +0200

    create entities

---
+++ lib/features/home/domain/entities/entities.dart
@@ -0,0 +1,15 @@
+class PrayerTimeEntity {
+  final String fajr;
+  final String duhr;
+  final String asr;
+  final String maghrib;
+  final String isha;
+
+  PrayerTimeEntity({
+    required this.fajr,
+    required this.duhr,
+    required this.asr,
+    required this.maghrib,
+    required this.isha,
+  });
+}
