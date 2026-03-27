fd5a317a51653c53f6a944cef720a09eea003e6d
khaledelnagdy10 <128099437+khaledelnagdy10@users.noreply.github.com>
2026-03-27 22:41:28 +0200

    refactor type of data

--- lib/features/home/data/repo/prayer_time_repo_imp.dart
+++ lib/features/home/data/repo/prayer_time_repo_imp.dart
@@ -1,4 +1,5 @@
 import 'package:mwazbet_elsalah/features/home/data/api/api.dart';
+import 'package:mwazbet_elsalah/features/home/data/models/prayer_time_model.dart';
 import 'package:mwazbet_elsalah/features/home/domain/entities/entities.dart';
 import 'package:mwazbet_elsalah/features/home/domain/repos/prayer_time_repo.dart';
 
@@ -10,12 +11,6 @@ class PrayerTimeRepoImp implements PrayerTimeRepo {
     final prayerTime = await api.getPrayerTime(city: city);
     if (prayerTime.isEmpty) return null; // حماية لو الـ API فشل
 
-    return PrayerTimeEntity(
-      fajr: prayerTime['Fajr'] as String,
-      duhr: prayerTime['Dhuhr'] as String,
-      asr: prayerTime['Asr'] as String,
-      maghrib: prayerTime['Maghrib'] as String,
-      isha: prayerTime['Isha'] as String,
-    );
+    return PrayerTimeModel.fromJson(prayerTime);
   }
 }
