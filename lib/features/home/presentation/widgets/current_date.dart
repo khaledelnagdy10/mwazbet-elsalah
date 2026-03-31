b0cfd48384e32e59334a4acce25fce2c0d30c429
khaledelnagdy10 <128099437+khaledelnagdy10@users.noreply.github.com>
2026-04-01 01:04:24 +0200

    add selected date

--- lib/features/home/presentation/widgets/current_date.dart
+++ lib/features/home/presentation/widgets/current_date.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 import 'package:mwazbet_elsalah/constants.dart';
+import 'package:mwazbet_elsalah/features/home/presentation/controller/prayer_time_cubit.dart';
 
 class CurrentDate extends StatelessWidget {
   const CurrentDate({super.key});
   @override
   Widget build(BuildContext context) {
-    DateTime currentDate = DateTime.now();
-    String formattedDate = DateFormat('d MMMM yyyy').format(currentDate);
+    DateTime currentDate = context.read<PrayerTimeCubit>().selectedDate;
+    String formattedDate = DateFormat('EEEE, d MMM yyyy').format(currentDate);
 
     return Text(formattedDate, style: Style.text12Grey(context));
   }
