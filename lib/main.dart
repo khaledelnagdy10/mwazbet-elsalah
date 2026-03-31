d22df3bc02ad1911eab749e500b9cdcaf5b15cfd
khaledelnagdy10 <128099437+khaledelnagdy10@users.noreply.github.com>
2026-04-01 01:10:53 +0200

    add get time use case to bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:mwazbet_elsalah/features/home/data/api/api.dart';
 import 'package:mwazbet_elsalah/features/home/data/repo/prayer_time_repo_imp.dart';
 import 'package:mwazbet_elsalah/features/home/domain/use_cases/get_next_prayer.dart';
 import 'package:mwazbet_elsalah/features/home/domain/use_cases/get_prayer_time.dart';
+import 'package:mwazbet_elsalah/features/home/domain/use_cases/get_remaining_time.dart';
 import 'package:mwazbet_elsalah/features/home/presentation/controller/prayer_time_cubit.dart';
 import 'package:mwazbet_elsalah/features/home/presentation/view/home_view.dart';
 
@@ -24,6 +25,7 @@ class MainApp extends StatelessWidget {
           prayerTimeRepo: PrayerTimeRepoImp(api: Api(dio: Dio())),
         ),
         getNextPrayer: GetNextPrayer(),
+        getRemainingTime: GetRemainingTimeUseCase(),
       ),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
