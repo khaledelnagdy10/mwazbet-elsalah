e0eb683b91d29d3105001c2f6fc6d7f301fd6d70
khaledelnagdy10 <128099437+khaledelnagdy10@users.noreply.github.com>
2026-04-01 01:09:53 +0200

    add counter time

--- lib/features/home/presentation/widgets/next_prayer_data.dart
+++ lib/features/home/presentation/widgets/next_prayer_data.dart
@@ -11,33 +11,49 @@ class NextPrayerData extends StatelessWidget {
     return BlocBuilder<PrayerTimeCubit, PrayerTimeState>(
       builder: (context, state) {
         if (state is PrayerTimeFailure) {
-          return Center(child: Text('ERROR while get next prayer data'));
+          return const Center(child: Text('ERROR while get next prayer data'));
         }
+
         if (state is PrayerTimeSuccess) {
+          final remaining = state.remaining;
+
+          String format(Duration d) {
+            String twoDigits(int n) => n.toString().padLeft(2, '0');
+
+            return "${twoDigits(d.inHours)}:"
+                "${twoDigits(d.inMinutes.remainder(60))}:"
+                "${twoDigits(d.inSeconds.remainder(60))}";
+          }
+
           return Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
-              Positioned(
-                child: Text(
-                  'Next Prayer',
-                  style: Style.bold20AllWhite(context),
-                ),
-              ),
+              Text('Next Prayer', style: Style.bold20AllWhite(context)),
+
+              const SizedBox(height: 2),
+
               Divider(
                 color: Colors.grey,
                 thickness: 1,
                 indent: 125,
                 endIndent: 125,
               ),
+
+              const SizedBox(height: 4),
+
               Text(
-                state.nextPrayers.prayerName,
+                state.nextPrayer.prayerName,
                 style: Style.bold30AllWhite(context),
               ),
-              Text('3:30:20', style: Style.bold30AllWhite(context)),
+
+              const SizedBox(height: 4),
+
+              Text(format(remaining), style: Style.bold30AllWhite(context)),
             ],
           );
         }
 
-        return SizedBox();
+        return const SizedBox();
       },
     );
   }
